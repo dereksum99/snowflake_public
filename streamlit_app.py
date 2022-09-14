325ace68ea10f9ec704460bea8cbbdceb44f75a3
dereksum99 <113507383+dereksum99@users.noreply.github.com>
2022-09-14 16:33:16 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,10 @@ streamlit.text('🥗Omasaki Burger')
 streamlit.text('🐔Hearty Cereal')
 streamlit.text('🥑🍞Fish and Chips made from UK London original')
 streamlit.text('🥑Avocado king fisher french fries')
+
+import pandas
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+streamlit.dataframe(my_fruit_list)
