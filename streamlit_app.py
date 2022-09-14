aba730858689ceefec6b77b1043254cb87357de9
dereksum99 <113507383+dereksum99@users.noreply.github.com>
2022-09-14 16:23:53 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My parents new healthy diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omasaki Burger')
+streamlit.text('Hearty Cereal')
+streamlit.text('Fish and Chips made from UK London original')
